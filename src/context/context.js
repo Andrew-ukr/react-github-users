0a017785d83f896635e5821b8dcd4d6186b4fb95
Andrii <pashulyaand@gmail.com>
2021-05-01 11:20:14 +0300

    created useContext

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect, createContext } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+export const GitHubContext = createContext();
+
+export const GitHubProvider = ({ children }) => {
+  return (
+    <GitHubContext.Provider value={"hello"}>{children}</GitHubContext.Provider>
+  );
+};
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,11 @@ import { GithubContext } from '../context/context';
 const Dashboard = () => {
   return (
     <main>
+      <Navbar></Navbar>
+      <Search></Search>
+      <Info></Info>
+      <User></User>
+      <Repos></Repos>
       <h2>Dashboard Page</h2>
     </main>
   );
