9805dae5b1c067ac78da304cad9844d5c885c3e3
Andrii <pashulyaand@gmail.com>
2021-05-02 08:51:53 +0300

    added tag form with input and submit button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
+
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setuser] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch></MdSearch>
+            <input
+              type="search"
+              name="search"
+              placeholder="enter github user"
+              value={user}
+              onChange={(e) => setuser(e.target.value)}
+            />
+            <button type="submit">search</button>
+          </div>
+        </form>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
